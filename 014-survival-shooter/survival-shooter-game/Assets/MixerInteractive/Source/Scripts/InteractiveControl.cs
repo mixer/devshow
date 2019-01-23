5343dffd07d062841811cf552906dd29abb1ab34
Mike Blouin <mb@xbox.com>
2019-01-23 08:14:20 -0800

    episode 14

---
+++ 014-survival-shooter/survival-shooter-game/Assets/MixerInteractive/Source/Scripts/InteractiveControl.cs
@@ -0,0 +1,181 @@
+﻿/*
+ * Mixer Unity SDK
+ *
+ * Copyright (c) Microsoft Corporation
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+using System.Collections.Generic;
+
+namespace Microsoft.Mixer
+{
+    /// Base class for Interactivity controls. All controls are created and 
+    /// configured using Interactive Studio.
+    /// </summary>
+#if !WINDOWS_UWP
+    [System.Serializable]
+#endif
+    public class InteractiveControl
+    {
+        /// <summary>
+        /// Unique string identifier for this control
+        /// </summary>
+        public string ControlID
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Indicates if control is enabled or disabled.
+        /// </summary>
+        public bool Disabled
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// The string of text that displays when a stream viewer (InteractiveParticipant) hovers over the control.
+        /// </summary>
+        public string HelpText
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Returns a read only dictionary of name, value pairs for the control's metadata.
+        /// </summary>
+        public IDictionary<string, object> MetaProperties
+        {
+            get;
+            private set;
+        }
+
+        internal string _eTag;
+        internal string _sceneID;
+        internal string _kind;
+        internal InteractiveEventType _type;
+        internal int participantID;
+
+        /// <summary>
+        /// Allow game client to disable a control.
+        /// </summary>
+        /// <param name="disabled">If "true", disables this control. If "false", enables this control</param>
+        public void SetDisabled(bool disabled)
+        {
+            Disabled = disabled;
+            InteractivityManager.SingletonInstance._SendSetButtonControlProperties(
+                ControlID, 
+                InteractivityManager._WS_MESSAGE_VALUE_DISABLED, 
+                disabled,
+                0,
+                string.Empty,
+                0);
+        }
+
+        /// <summary>
+        /// This function allows you to set any property on a control.
+        /// </summary>
+        /// <param name="name">The name of the control property.</param>
+        /// <param name="value">The value of the control property.</param>
+        public void SetProperty(InteractiveControlProperty name, bool value)
+        {
+            SetPropertyImpl(
+                InteractivityManager.SingletonInstance._InteractiveControlPropertyToString(name),
+                value
+                );
+        }
+        public void SetProperty(InteractiveControlProperty name, double value)
+        {
+            SetPropertyImpl(
+                InteractivityManager.SingletonInstance._InteractiveControlPropertyToString(name),
+                value
+                );
+        }
+        public void SetProperty(InteractiveControlProperty name, string value)
+        {
+            SetPropertyImpl(
+                InteractivityManager.SingletonInstance._InteractiveControlPropertyToString(name),
+                value
+                );
+        }
+        public void SetProperty(InteractiveControlProperty name, object value)
+        {
+            SetPropertyImpl(
+                InteractivityManager.SingletonInstance._InteractiveControlPropertyToString(name), 
+                value
+                );
+        }
+
+        /// <summary>
+        /// This function allows you to set any property on a control.
+        /// </summary>
+        /// <param name="name">The name of the control property.</param>
+        /// <param name="value">The value of the control property.</param>
+        public void SetProperty(string name, bool value)
+        {
+            SetPropertyImpl(name, value);
+        }
+        public void SetProperty(string name, double value)
+        {
+            SetPropertyImpl(name, value);
+        }
+        public void SetProperty(string name, string value)
+        {
+            SetPropertyImpl(name, value);
+        }
+        public void SetProperty(string name, object value)
+        {
+            SetPropertyImpl(name, value);
+        }
+
+        private void SetPropertyImpl(string name, bool value)
+        {
+            InteractivityManager.SingletonInstance._QueuePropertyUpdate(_sceneID, ControlID, name, value);
+        }
+        private void SetPropertyImpl(string name, double value)
+        {
+            InteractivityManager.SingletonInstance._QueuePropertyUpdate(_sceneID, ControlID, name, value);
+        }
+        private void SetPropertyImpl(string name, string value)
+        {
+            InteractivityManager.SingletonInstance._QueuePropertyUpdate(_sceneID, ControlID, name, value);
+        }
+        private void SetPropertyImpl(string name, object value)
+        {
+            InteractivityManager.SingletonInstance._QueuePropertyUpdate(_sceneID, ControlID, name, value);
+        }
+
+        internal InteractiveControl(string controlID, string kind, InteractiveEventType type, bool disabled, string helpText, string eTag, string sceneID, Dictionary<string, object> metaProperties)
+        {
+            ControlID = controlID;
+            _kind = kind;
+            _type = type;
+            Disabled = disabled;
+            HelpText = helpText;
+            _eTag = eTag;
+            _sceneID = sceneID;
+            participantID = -1;
+            MetaProperties = metaProperties;
+        }
+    }
+}
---
+++ 014-survival-shooter/survival-shooter-game/Assets/_Complete-Game/Scripts/InteractiveManager.cs
@@ -0,0 +1,258 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CompleteProject
+{
+    [Serializable]
+    class InteractiveBombMessage {
+        public string bombType;
+        public InteractiveBombLocation location;
+    }
+
+    [Serializable]
+    class InteractiveBombLocation
+    {
+        public float x;
+        public float y;
+    }
+
+    [Serializable]
+    class InteractivePacket<T>
+    {
+        public string method;
+        public InteractiveInputParams @params;
+    }
+
+    [Serializable]
+    class InteractiveInputParams
+    {
+        public string participantID;
+        public InteractiveBombMessage input;
+    }
+
+    public class InteractiveManager : MonoBehaviour
+    {
+        public const float UpdateMovementEpsilon = 0.01f;
+
+        public float BlastBombRadius = 1.0f;
+        public int BlastBombDamage = 50;
+
+        public float SonicBombRadius = 1.0f;
+        public MovementAffector SonicBombAffector;
+
+        public GameObject[] enemies;
+        public GameObject player;
+
+        private Dictionary<GameObject, Point> lastUpdatedPoints = new Dictionary<GameObject, Point>();
+        private List<GameObject> removeList = new List<GameObject>();
+
+
+        private float lastSendTime = -1;
+        private float sendInterval = 0.1f;
+
+        // Use this for initialization
+        void Start()
+        {
+            MixerInteractive.GoInteractive();
+            MixerInteractive.OnInteractiveMessageEvent += MixerInteractive_OnInteractiveMessageEvent;
+        }
+
+        private void MixerInteractive_OnInteractiveMessageEvent(object sender, Microsoft.Mixer.InteractiveMessageEventArgs e)
+        {
+            if (!e.Message.Contains("giveInput"))
+            {
+                return;
+            }
+
+            var message = JsonUtility.FromJson<InteractivePacket<InteractiveBombMessage>>(e.Message);
+            if (null == message.@params || null == message.@params.input)
+            {
+                return;
+            }
+
+            var location = message.@params.input.location;
+            RaycastHit hit;
+            var ray = Camera.main.ViewportPointToRay(new Vector3(
+                Util.ScaleValue(location.x, 0, 1, Util.MinimapMinBoundary.x, Util.MinimapMaxBoundary.x),
+                Util.ScaleValue(1 - location.y, 0, 1, Util.MinimapMinBoundary.y, Util.MinimapMaxBoundary.y),
+                0));
+
+            if (!Physics.Raycast(ray, out hit))
+            {
+                Debug.LogError("Bomb missed world");
+                return;
+            }
+
+            switch (message.@params.input.bombType)
+            {
+                case "blast":
+                    new BlastBomb(BlastBombRadius, BlastBombDamage)
+                            .Drop(hit.point);
+                    break;
+                case "sonic":
+                    new SonicBomb(SonicBombRadius, SonicBombAffector)
+                        .Drop(hit.point);
+                    break;
+                default:
+                    Debug.LogError("I'm not dropping it like it's hot: " + message.@params.input.bombType);
+                    break;
+            }
+        }
+
+        [ContextMenu("Drop Test Blast Bomb")]
+        void DropTestBlastBomb()
+        {
+            var player = GameObject.FindGameObjectWithTag("Player");
+            if (null != player)
+            {
+                new BlastBomb(BlastBombRadius, BlastBombDamage)
+                    .Drop(player.transform.position);
+            }
+        }
+
+        [ContextMenu("Drop Test Sonic Bomb")]
+        void DropTestSonicBomb()
+        {
+            var player = GameObject.FindGameObjectWithTag("Player");
+            if (null == player)
+            {
+                throw new Exception("No player in scene");
+            }
+
+            var bomb = new SonicBomb(SonicBombRadius, SonicBombAffector);
+            bomb.Drop(player.transform.position);
+        }
+
+        void Update()
+        {
+            if (Time.time - lastSendTime < sendInterval)
+            {
+                return;
+            }
+
+            lastSendTime = Time.time;
+
+            enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            player = GameObject.FindGameObjectWithTag("Player");
+
+            var updateList = new Dictionary<int, Point>();
+
+            // Send up points
+            var point = PointFromGameObject(player);
+            if (null != point)
+            {
+                updateList.Add(player.GetInstanceID(), point);
+            }
+
+            foreach (GameObject enemy in enemies)
+            {
+                var enemyPoint = PointFromGameObject(enemy);
+                var oldPoint = lastUpdatedPoints.ContainsKey(enemy)
+                    ? lastUpdatedPoints[enemy]
+                    : null;
+                if (null != enemyPoint && (null == oldPoint || IsPointUpdated(enemyPoint, oldPoint)))
+                {
+                    updateList.Add(enemy.GetInstanceID(), enemyPoint);
+                    TrackEnemyPoint(enemyPoint, enemy);
+                }
+            }
+
+            foreach (var enemy in removeList)
+            {
+                updateList.Add(enemy.GetInstanceID(), null);
+            }
+
+            removeList.Clear();
+
+            if (updateList.Count > 1)
+            {
+                string pointUpdateMessage = "{" +
+                        "\"type\": \"method\"," +
+                        "\"id\": 123,  " +
+                        "\"method\": \"broadcastEvent\"," +
+                        "\"params\": {      " +
+                        "    \"scope\": [   " +
+                        "    \"group:default\"" +
+                        "      ],  " +
+                        "      \"data\": " + JsonConvert.SerializeObject(updateList) +
+                        "    }, " +
+                        "    \"discard\": true" +
+                        "  }";
+
+                if (MixerInteractive.InteractivityState == Microsoft.Mixer.InteractivityState.InteractivityEnabled)
+                {
+                    MixerInteractive.SendInteractiveMessage(pointUpdateMessage);
+                }
+            }
+        }
+
+        private bool IsPointUpdated(Point newPoint, Point oldPoint)
+        {
+            return newPoint.enemy != oldPoint.enemy
+                || newPoint.colorHex != oldPoint.colorHex
+                || newPoint.size != oldPoint.size
+                || Mathf.Abs(newPoint.x - oldPoint.x) > UpdateMovementEpsilon
+                || Mathf.Abs(newPoint.y - oldPoint.y) > UpdateMovementEpsilon;
+        }
+
+        private void TrackEnemyPoint(Point point, GameObject enemy)
+        {
+            if (!lastUpdatedPoints.ContainsKey(enemy))
+            {
+                var healthManager = enemy.GetComponent<EnemyHealth>();
+                if (null == healthManager)
+                {
+                    // Can't track
+                    return;
+                }
+
+                healthManager.EnemyDestroyed += HealthManager_EnemyDestroyed;
+            }
+
+            lastUpdatedPoints[enemy] = point;
+        }
+
+        private void HealthManager_EnemyDestroyed(GameObject sender)
+        {
+            removeList.Add(sender);
+        }
+
+        private Point PointFromGameObject(GameObject obj)
+        {
+            var newPosition = Camera.main.WorldToViewportPoint(obj.transform.position);
+
+            if (
+                newPosition.x > Util.MinimapMinBoundary.x
+                && newPosition.x < Util.MinimapMaxBoundary.x
+                && newPosition.y > Util.MinimapMinBoundary.y
+                && newPosition.y < Util.MinimapMaxBoundary.y
+            )
+            {
+                // Default
+                float size = 1.0f;
+                Color color = Color.black;
+                bool isEnemy = true;
+
+                var mmHelper = obj.GetComponent<MinimapHelper>();
+                if (null != mmHelper)
+                {
+                    color = mmHelper.MinimapColor;
+                    size = mmHelper.MinimapSize;
+                    isEnemy = mmHelper.IsEnemy;
+                }
+
+                return new Point(
+                    Util.ScaleValue(newPosition.x, Util.MinimapMinBoundary.x, Util.MinimapMaxBoundary.x, 0, 1),
+                    Util.ScaleValue(newPosition.y, Util.MinimapMinBoundary.y, Util.MinimapMaxBoundary.y, 0, 1),
+                    isEnemy,
+                    color,
+                    size);
+            }
+
+            return null;
+        }
+    }
+}
